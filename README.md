bfec9e4ab3655a4ae1b050553559004c01fe9da6
takipsizad <addeniz25@gmail.com>
2021-02-15 13:36:39 +0300

    Revert "Revert "Initial commit""
    This reverts commit e98bb78db3352e3ede0a8e1a6898bfc30aa0c8c8.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ethapi
+ idk
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+var Web3 = require('web3');
+var isEmpty = require('lodash.isempty');
+var web3 = new Web3(process.env.wsurl);
+var oof = 'i messed up or you messed up dont crash my site smh '
+
+router.get('/', function(req, res) {
+  res.sendFile( path.resolve('./views/index.html') );
+});
+router.get('/api/gasprices', function(req, res, next) {
+  web3.eth.getGasPrice().then(result => { 
+    res.jsonp({gasprices:result})
+  });
+});
+router.get('/api/version', function(req, res, next) {
+  res.jsonp({web3version:web3.version})
+});
+router.get('/api/checkbal', function(req, res, next) {
+  web3.eth.getBalance(req.query.wallet).then(result => { 
+    res.jsonp({balance:result})
+  });
+});
+router.get('/api/ibantoadress', function(req, res) {
+  res.jsonp({adress:web3.eth.Iban.toAddress(req.query.Iban)})
+});
+router.get('/api/adresstoiban', function(req, res) {
+  res.jsonp({iban:web3.eth.Iban.toIban(req.query.adress)})
+});
+router.get('/api/getblock', function(req, res, next) {
+  web3.eth.getBlock(req.query.blockid).then(result => { 
+    res.jsonp({result})
+  }).catch(err => {res.jsonp({oof})});
+});
+
+router.get('/api/transactioninfo', function(req, res, next) {
+  web3.eth.getTransaction(req.query.hash).then(result => { 
+    res.jsonp({result})
+  }).catch(err => {res.jsonp({oof})});
+});
+module.exports = router;
