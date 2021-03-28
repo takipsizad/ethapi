b7e1d4d595972f6c19056db357b41e330e2fc711
takipsizad <addeniz25@gmail.com>
2021-03-28 13:01:30 +0300

    idk

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,96 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 var Web3 = require('web3');
-var web3 = new Web3(process.env.wsurl);
+var eth1 = new Web3(process.env.eth1url);
+//var eth2 = new Web3(process.env.eth2url);
 var Accounts = require('web3-eth-accounts');
-var accounts = new Accounts(process.env.wsurl);
+var accounts1 = new Accounts(process.env.eth1url);
+//var accounts2 = new Accounts(process.env.eth2url);
 var oof = 'i messed up or you messed up dont crash my site smh '
 
 router.get('/', function(req, res) {
   res.sendFile( path.resolve('./views/index.html') );
 });
-router.get('/api/gasprices', function(req, res, next) {
-  web3.eth.getGasPrice().then(result => { 
+
+
+router.get('/api/v1/gasprices', function(req, res, next) {
+  eth1.eth.getGasPrice().then(result => { 
     res.jsonp({gasprices:result})
   });
 });
-router.get('/api/version', function(req, res, next) {
+//router.get('/api/v2/gasprices', function(req, res, next) {
+//  eth2.eth.getGasPrice().then(result => { 
+//    res.jsonp({gasprices:result})
+//  });
+//});
+
+
+router.get('/api/v1/version', function(req, res, next) {
   res.jsonp({web3version:web3.version})
 });
-router.get('/api/checkbal', function(req, res, next) {
-  web3.eth.getBalance(req.query.wallet).then(result => { 
+//router.get('/api/v2/version', function(req, res, next) {
+//  res.jsonp({web3version:web3.version})
+//});
+
+
+router.get('/api/v1/checkbal', function(req, res, next) {
+  eth1.eth.getBalance(req.query.wallet).then(result => { 
     res.jsonp({balance:result})
   });
 });
-router.get('/api/ibantoadress', function(req, res) {
-  res.jsonp({adress:web3.eth.Iban.toAddress(req.query.Iban)})
+//router.get('/api/v2/checkbal', function(req, res, next) {
+//  eth2.eth.getBalance(req.query.wallet).then(result => { 
+//    res.jsonp({balance:result})
+//  });
+//});
+
+
+router.get('/api/v1/ibantoadress', function(req, res) {
+  res.jsonp({adress:eth1.eth.Iban.toAddress(req.query.Iban)})
 });
-router.get('/api/adresstoiban', function(req, res) {
-  res.jsonp({iban:web3.eth.Iban.toIban(req.query.adress)})
+//router.get('/api/v2/ibantoadress', function(req, res) {
+//  res.jsonp({adress:eth2.eth.Iban.toAddress(req.query.Iban)})
+//});
+
+
+router.get('/api/v1/adresstoiban', function(req, res) {
+  res.jsonp({iban:eth1.eth.Iban.toIban(req.query.adress)})
 });
-router.get('/api/getblock', function(req, res, next) {
-  web3.eth.getBlock(req.query.blockid).then(result => { 
+//router.get('/api/v2/adresstoiban', function(req, res) {
+//  res.jsonp({iban:eth2.eth.Iban.toIban(req.query.adress)})
+//});
+
+
+
+router.get('/api/v1/getblock', function(req, res, next) {
+  eth1.eth.getBlock(req.query.blockid).then(result => { 
     res.jsonp({result})
   }).catch(err => {res.jsonp({oof})});
 });
+//router.get('/api/v2/getblock', function(req, res, next) {
+//  eth2.eth.getBlock(req.query.blockid).then(result => { 
+//    res.jsonp({result})
+//  }).catch(err => {res.jsonp({oof})});
+//});
 
-router.get('/api/transactioninfo', function(req, res, next) {
+router.get('/api/v1/transactioninfo', function(req, res, next) {
   web3.eth.getTransaction(req.query.hash).then(result => { 
     res.jsonp({result})
   }).catch(err => {res.jsonp({oof})});
 });
+//router.get('/api/v2/transactioninfo', function(req, res, next) {
+//  web3.eth.getTransaction(req.query.hash).then(result => { 
+//    res.jsonp({result})
+//  }).catch(err => {res.jsonp({oof})});
+//});
 
-router.get('/api/createacc', function(req, res, next) {
-  var acc = web3.eth.accounts.create()
+router.get('/api/v1/createacc', function(req, res, next) {
+  var acc = eth1.eth.accounts.create()
   res.jsonp({acc})
 });
+//router.get('/api/v2/createacc', function(req, res, next) {
+//  var acc = eth2.eth.accounts.create()
+//  res.jsonp({acc})
+//});
+
 module.exports = router;
